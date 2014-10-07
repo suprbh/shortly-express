37d5c0998136b558c30440333ead8ec351f2ad04
suprbh <supb27@yahoo.com>
2014-10-06 17:04:37 -0700

    /login redirects to login but does not depend on a user's authentication.

--- shortly.js
+++ shortly.js
@@ -23,24 +23,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/',
+function(req, res) {
+  // if user is logged in,
+    res.render('index');
+  // else, redirect to /login
+  //  res.render('login');
+
+});
+
+app.get('/login', function(req, res){
+
+  res.send('<form method="post" action="/login">' +
+  '<p>' +
+    '<label>Username:</label>' +
+    '<input type="text" name="username">' +
+  '</p>' +
+  '<p>' +
+    '<label>Password:</label>' +
+    '<input type="password" name="password">' +
+  '</p>' +
+  '<p>' +
+    '<input type="submit" value="Login">' +
+  '</p>' +
+  '</form>');
+});
+
+app.get('/create',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
-function(req, res) {
-  res.render('index');
-});
-
-app.get('/links', 
+app.get('/links',
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+
+app.post('/links',
 function(req, res) {
   var uri = req.body.url;
 
@@ -87,6 +109,7 @@ function(req, res) {
 /************************************************************/
 
 app.get('/*', function(req, res) {
+
   new Link({ code: req.params[0] }).fetch().then(function(link) {
     if (!link) {
       res.redirect('/');
